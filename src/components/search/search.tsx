39ae9a4dfd8f7a00faec844632c440fb8cdfa998
Maxim <MaximKomoza@yandex.ru>
2024-01-03 15:37:51 +0700

    sorting button

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,12 +1,15 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { getUsers } from '../../api/api';
 import './search.css';
 import { useDispatch } from 'react-redux';
 import { setUsers } from '../../store/actions/creators';
 
+const sorting = ['По умолчанию', 'Репозитории (убыв)', 'Репозитории (возр)'];
+
 export const Search = () => {
     const dispatch = useDispatch();
     const refInput = useRef<HTMLInputElement | null>(null);
+    const [currSort, setCurrSort] = useState<string>(sorting[0]);
 
     const handleClickSubmit = (
         e: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -16,7 +19,6 @@ export const Search = () => {
             if (refInput.current.value) {
                 getUsers(refInput.current.value)
                     .then((data) => {
-                        console.log(data);
                         dispatch(setUsers(data));
                     })
                     .catch(() => {
@@ -26,6 +28,17 @@ export const Search = () => {
                 dispatch(setUsers(null));
             }
     };
+
+    const handleClickSorting = () => {
+        const idx = sorting.indexOf(currSort);
+
+        if (idx !== sorting.length - 1) {
+            setCurrSort(sorting[idx + 1]);
+        } else {
+            setCurrSort(sorting[0]);
+        }
+    };
+
     return (
         <search>
             <form className="search">
@@ -43,6 +56,15 @@ export const Search = () => {
                     Найти
                 </button>
             </form>
+            <div className="search__sort">
+                <p className="search__sort-text">Сортировка:</p>
+                <button
+                    onClick={handleClickSorting}
+                    className="search__sort-button"
+                >
+                    {currSort}
+                </button>
+            </div>
         </search>
     );
 };
